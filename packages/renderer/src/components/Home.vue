af39a5f28182a9ab85e2624c4fb92c08fe2e0556
Cno <dsyourshy@qq.com>
2022-05-03 19:28:30 +0800

    界面微调

--- packages/renderer/src/components/Home.vue
+++ packages/renderer/src/components/Home.vue
@@ -1,24 +1,62 @@
 <template>
-  <div id="container">
-    <a-input-search
-        v-model:value="url"
-        :loading="loading"
-        enter-button="解析"
-        placeholder="输入小游戏网址"
-        size="large"
-        @search="parse"
-    >
-      <template #suffix>
-        <close-circle-outlined v-show="url!==''" style="color: rgba(0,0,0,0.45)" @click="url=''"/>
-      </template>
-    </a-input-search>
-    <a-descriptions v-if="displayInfo!=null" bordered>
-      <template #title>
-        <h2>小游戏信息</h2>
-        <a-button primary @click="download">下载</a-button>
-      </template>
-      <a-descriptions-item v-for="item of displayInfo" :label="item.title">{{ item.value }}</a-descriptions-item>
-    </a-descriptions>
+  <div class="container">
+    <a-row style="margin-top: 15%;margin-bottom: 5%" type="flex">
+      <a-col :span="10"/>
+      <a-col :span="6">
+        <a-radio-group
+            v-model:value="inputType"
+            :bordered="false"
+        >
+          <a-radio-button :value="0">标准</a-radio-button>
+          <a-radio-button :value="1">批量</a-radio-button>
+          <a-radio-button :value="2">搜索</a-radio-button>
+        </a-radio-group>
+      </a-col>
+    </a-row>
+    <a-row type="flex">
+      <a-col :span="2"/>
+      <a-col :span="17">
+        <a-input
+            v-model:value="url"
+            allowClear
+            placeholder="粘贴小游戏网址"
+            size="large"
+            @input="parse"
+        >
+          <template v-if="gameInfo!=null&&url!==''" #suffix>
+            <p style="color: rgba(0,0,0,0.45);margin-bottom: 0">{{ gameInfo.title }}</p>
+          </template>
+        </a-input>
+      </a-col>
+      <a-col :span="1"/>
+      <a-col :span="3">
+        <a-button
+            :disabled="buttonDisabled"
+            :loading="loading"
+            size="large"
+            type="primary"
+        >{{ buttonText }}
+        </a-button>
+      </a-col>
+    </a-row>
+
+    <a-row style="height: 20%;padding-top: 10%">
+      <a-col :span="10"/>
+      <a-col :span="4">
+        <a-space class="status-bar" size="middle">
+          7k7k
+          <check-circle-outlined style="color: #42b983"/>
+          <a-button size="small">登出</a-button>
+        </a-space>
+
+        <a-space class="status-bar" size="middle">
+          4399
+          <close-circle-outlined style="color: gray"/>
+          <a-button size="small">登录</a-button>
+        </a-space>
+      </a-col>
+    </a-row>
+
   </div>
 </template>
 
@@ -29,56 +67,81 @@ import {ipcRenderer} from "electron";
 import {Result} from "ts-results";
 import {GameInfo} from "../../../class";
 import {message} from 'ant-design-vue';
-import {CloseCircleOutlined} from '@ant-design/icons-vue';
+import {CheckCircleOutlined, CloseCircleOutlined} from '@ant-design/icons-vue';
 
 const route = useRoute()
-let url = ref<string>(""), displayInfo = ref<Array<{ title: string, value: string }> | null>(null),
-    loading = ref<boolean>(false)
-let gameInfo: GameInfo | null = null
+let url = ref<string>(""),
+    loading = ref<boolean>(false),
+    buttonDisabled = ref<boolean>(true),
+    inputType = ref<number>(0),
+    buttonText = ref<string>("下载"),
+    gameInfo = ref<GameInfo | null>(null)
 
 //监听解析结果返回
 ipcRenderer.on('parse-reply', (event, result: Result<GameInfo, string>) => {
   console.log(result)
   if (result.ok) {
-    gameInfo = result.val
-    displayInfo.value = [
-      {title: "名称", value: gameInfo.title},
-      {title: "分类", value: gameInfo.category},
-      {title: "类型", value: gameInfo.type},
-      {title: "原网址", value: gameInfo.online.originPage},
-      {title: "真实网址", value: gameInfo.online.truePage},
-      {title: "游戏文件下载链接", value: gameInfo.online.binUrl},
-    ]
+    buttonDisabled.value = false
+    gameInfo.value = result.val
   } else {
     message.error(result.val)
-    displayInfo.value = null
   }
   loading.value = false
 })
 
+const urlRegex = /https?:\/\/\S+\.html?/
+let recentSubmit = 0
+
 function parse() {
+  //判断url是否符合提交要求
+  if (url.value == "" || !urlRegex.test(url.value)) return
+
+  //防抖
+  if (Date.now() - recentSubmit < 1000) return
+
+  //提交搜索请求
+  recentSubmit = Date.now()
   loading.value = true
   ipcRenderer.send('parse', url.value)
 }
 
 //监听下载进度事件更新
 ipcRenderer.on('download-progress', (event, payload: { gameInfo: GameInfo, percentage: number }) => {
+  buttonText.value = payload.percentage + "%"
   console.log(payload)
 })
 
 ipcRenderer.on('download-reply', (event, payload: GameInfo) => {
+  buttonDisabled.value = false
+  buttonText.value = "下载"
+  url.value = ""
   console.log('finish')
   console.log(payload)
 })
 
 function download() {
+  buttonDisabled.value = true
+  buttonText.value = "0%"
   ipcRenderer.send('download', gameInfo)
 }
 </script>
 
 <style scoped>
-#container {
+.logo-big {
+  width: 15vw;
+  height: 15vw;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  align-content: center;
+}
+
+.status-bar {
+  margin-top: 5%;
+  margin-bottom: 5%;
 }
 </style>
