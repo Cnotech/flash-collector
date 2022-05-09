6b4cb0b938f6e6a2443e8e77e1446a910d55dfc4
Cno <dsyourshy@qq.com>
2022-05-10 01:25:50 +0800

    改变主题色

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,9 +1,15 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import './samples/node-api'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import Antd, {ConfigProvider} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.variable.min.css';
 import {router} from "./router";
+
+ConfigProvider.config({
+    theme: {
+        primaryColor: '#4bb117',
+    },
+});
 
 createApp(App)
     .use(Antd)
