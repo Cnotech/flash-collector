3d4c1440a2721e6f69ed5d6a8a2f33aa7adb49c0
Cno <dsyourshy@qq.com>
2022-05-19 15:01:27 +0800

    初始化更新相关

---
+++ retinue/update/main.cmd
@@ -0,0 +1,28 @@
+@echo off
+title Flash Collector Hot Update
+color 3f
+
+if not exist flash-collector.exe (
+    echo Error working directory
+    echo %~dp0
+    pause
+    exit
+)
+if "%1" == "" (
+    echo Empty argument
+    pause
+    exit
+)
+
+echo Waiting for main program evacuation...
+@ping 127.0.0.1 -n 2 >nul
+TASKKILL /F /IM flash-collector.exe /T >nul
+
+xcopy /s /r /y .\UPDATE-TEMP\* .\
+.\retinue\update\rcedit-x64.exe flash-collector.exe --set-file-version "%1%"
+.\retinue\update\rcedit-x64.exe flash-collector.exe --set-product-version "%1%"
+
+echo Flash Collector updated successfully
+@ping 127.0.0.1 -n 3 >nul
+del /f /q main.cmd
+del /f /q "%0"
